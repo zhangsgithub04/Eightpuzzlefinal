8c831c8110905e5f1157ea8b9128e4fc27990efb
zhangs <zhangsgm@gmail.com>
2017-03-24 16:54:31 -0400

    adding gyro, magnetic and accelorometer sensors

--- app/src/main/java/com/example/zhangs/eightpuzzle/MainActivity.java
+++ app/src/main/java/com/example/zhangs/eightpuzzle/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.zhangs.eightpuzzle;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView sView = (ImageView)findViewById(R.id.sv);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.splash);
+
+        Bitmap.Config config = bm.getConfig();
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+
+        Bitmap newImage = Bitmap.createBitmap(width, height, config);
+
+        Canvas c = new Canvas(newImage);
+        c.drawBitmap(bm, 0, 0, null);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(10);
+        c.drawText("CSCI 268 Spr. 2017 SUNY Oneonta", 0, 31, paint);
+
+        sView.setImageBitmap(newImage);
+
+
 
         final int SPLASH_TIME_OUT = 3000;  // 3 seconds
         new Handler().postDelayed(new Runnable() {
